aa8bc9dec22551e0af3df8caad981cfc0c8e675b
unknown <rivkizolti@gmail.com>
2021-07-07 11:55:30 +0300

    join

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    index=3
+    x="C"
+    print_hi('PyCharm')
+    str="ACCTGGA"
+    str=str[:index:]+x +str[index+1::]
+    print(str)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
